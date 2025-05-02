3dbc40062359c3033419a0cfe52c58af99003977
insafali05 <insaf.ali0825@gmail.com>
2025-05-02 13:14:29 +0530

    database seed

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Activity;
+use App\Models\UserPoint;
 
 class User extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['full_name'];
+
+
+    public function activities()
+    {
+        return $this->hasMany(Activity::class);
+    }
+    public function userPoint()
+    {
+        return $this->hasOne(UserPoint::class);
+    }
 }
--- app/Models/UserPoint.php
+++ app/Models/UserPoint.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class UserPoint extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'total_points', 'rank'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
